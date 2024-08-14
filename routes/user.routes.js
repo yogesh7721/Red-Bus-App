fef788fb4b251d02ea04bdfb87daaf08a746e5b9
yogesh shelke <shelkeyogesh874@gmail.com>
2024-08-14 16:24:34 +0530

    user regieter

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,7 @@ const router = require("express").Router()
 router
     .post("/register-user", userConroller.RegisterUser)
     .post("/login-user", userConroller.LoginUser)
-    .post("/user-otp", userConroller.VerifyOTPUser)
-    .post("/logout-user", userConroller.LogoutAdmin)
+// .post("/user-otp", userConroller.VerifyOTPUser)
+// .post("/logout-user", userConroller.LogoutAdmin)
 
 module.exports = router
