c7b6dcf9db6b619596567db307f44e45d737af24
yogesh shelke <shelkeyogesh874@gmail.com>
2024-08-29 16:02:21 +0530

    chages serv

--- controllers/busSeatBook.controller.js
+++ controllers/busSeatBook.controller.js
@@ -4,7 +4,7 @@ const asyncHandler = require("express-async-handler")
 const BusBookSeat = require("../models/BusBookSeat")
 
 exports.BookSeat = asyncHandler(async (req, res) => {
-    await BusBookSeat.create({ ...req.body, userId: loggedInUser })
+    await BusBookSeat.create({ ...req.body, userId: req.loggedInUser })
     res.json({ message: "Seat Book Success...." })
 })
 exports.GetBookSeat = asyncHandler(async (req, res) => {
