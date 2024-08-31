81456bff8009e05bc4985504c158243c75f359ec
yogesh shelke <shelkeyogesh874@gmail.com>
2024-08-31 16:35:02 +0530

    changes

--- controllers/busSeatBook.controller.js
+++ controllers/busSeatBook.controller.js
@@ -3,50 +3,21 @@
 const asyncHandler = require("express-async-handler")
 const BusBookSeat = require("../models/BusBookSeat")
 const sendEmail = require("../utils/email")
+const User = require("../models/User")
 
 exports.BookSeat = asyncHandler(async (req, res) => {
     const { date, from, to, provider, seats } = req.body
     await BusBookSeat.create({ ...req.body, userId: req.loggedInUser })
+    const userDetail = await User.findById(userId).exec()
+    console.log("username", userDetail.name);
+
     const styledServer = `
-    <html>
-    <head>
-        <style>
-            body {
-                font-family: 'Arial', sans-serif;
-                background-color: #f4f4f4;
-                color: #333;
-            }
-            h1 {
-                color: #007BFF;
-            }
-            p {
-                font-size: 16px;
-                line-height: 1.6;
-                color: #555;
-            }
-            .phone-number {
-                background-color: #87CEEB;
-                padding: 10px;
-                display: inline-block;
-                color: white;
-            }
-            .center-text {
-                text-align: left;
-            }
-            .left-text {
-                text-align: left;
-            }
-            .signature {
-                margin-top: 20px;
-                font-style: italic;
-            }
-        </style>
-    </head>
-    <body>
+<html>
+     <body>
         <p class="left-text" style="font-weight: bold;">Hello,</p>
-        <h4 class="center-text">
+        <h3 class="center-text">
         Your Seat Book Is Comform 💐 
-        </h4>
+        </h3>
         <p class="center-text">
            Date: ${date}
         </p>
@@ -63,7 +34,7 @@ exports.BookSeat = asyncHandler(async (req, res) => {
          Seats: ${seats}
         </p>
         <p class="center-text">
-            Join the workshop on time. Thank you.
+           Thank you.
         </p>
     </body>
     </html>`;
