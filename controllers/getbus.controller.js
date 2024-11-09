7d0ead3d86b7c95c92a8bf5c660358a49f145f3f
yogesh shelke <shelkeyogesh874@gmail.com>
2024-11-09 12:07:26 +0530

    searchbusadd

--- controllers/getbus.controller.js
+++ controllers/getbus.controller.js
@@ -2,6 +2,7 @@
 const asyncHandler = require("express-async-handler")
 const getBus = require("../models/getBus")
 const { checkEmpty } = require("../utils/checkEmpty")
+const { default: BusSeat } = require("../../mobile/screen/BusSeat")
 // const {  BusSeat } = require("../../mobile/screen/BusSeat")
 
 
@@ -13,4 +14,13 @@ exports.AddRoute = asyncHandler(async (req, res) => {
     }
     await getBus.create({ from, to, date, })
     res.status(200).json({ message: "Seat book success" })
+})
+exports.SearchBusses = asyncHandler(async (req, res) => {
+    const { from, to, } = req.body
+    const isfound = await BusSeat.find({ from, to })
+    if (!isfound) {
+        return res.status(400).json({ message: "Busses not found..." })
+    }
+    res.json({ message: "Bus find success...", result: isfound })
+
 })
--- routes/getbus.routes.js
+++ routes/getbus.routes.js
@@ -4,6 +4,7 @@ const router = require("express").Router()
 
 router
     .post("/Add-Route", getbusController.AddRoute)
+    .post("/search-busses", getbusController.SearchBusses)
 
 
 module.exports = router             
